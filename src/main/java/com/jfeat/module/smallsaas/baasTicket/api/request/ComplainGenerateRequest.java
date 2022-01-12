f12d85abc5ba882c792a56e5b002a93be734e34f
zchengb <zxchengb@163.com>
2022-01-12 13:53:13 +0800

    #N/A feat: add chain accessor in complain generate request.

--- src/main/java/com/jfeat/module/smallsaas/baasTicket/api/request/ComplainGenerateRequest.java
+++ src/main/java/com/jfeat/module/smallsaas/baasTicket/api/request/ComplainGenerateRequest.java
@@ -3,10 +3,12 @@ package com.jfeat.module.smallsaas.baasTicket.api.request;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.experimental.Accessors;
 
 @Data
 @Builder
 @AllArgsConstructor
+@Accessors(chain = true)
 public class ComplainGenerateRequest {
     private Long relationOrderId;
     private String title;
