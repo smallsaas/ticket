76d03ea0380698c87d5d40b554be34fa27baace0
SakuraRoad <754483487@qq.com>
2022-02-22 12:11:30 +0800

    add NoArgsConstructor in request

--- src/main/java/com/jfeat/module/smallsaas/ticket/api/request/ComplainGenerateRequest.java
+++ src/main/java/com/jfeat/module/smallsaas/ticket/api/request/ComplainGenerateRequest.java
@@ -3,11 +3,13 @@ package com.jfeat.module.smallsaas.ticket.api.request;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
 @Data
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 @Accessors(chain = true)
 public class ComplainGenerateRequest {
     private Long relationOrderId;
